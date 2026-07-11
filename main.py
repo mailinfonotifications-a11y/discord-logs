0e4d11ca7ad382aee3b643b8cee92afee7d27d8c
mailinfonotifications-a11y <mail.info.notifications@gmail.com>
2026-07-11 19:49:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ sheet = client.open_by_key(SPREADSHEET_ID)
 # 2. Discord Botのセットアップ
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_code="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
